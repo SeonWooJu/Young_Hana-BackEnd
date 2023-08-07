bc8904b695181c6e010d388c8570b2409265038d
SeonWooJu <tjsdnwn21104@gmail.com>
2023-08-08 04:50:53 +0900

    First Commit

---
+++ src/main/java/com/example/young_hanabackend/YoungHanaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.young_hanabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungHanaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoungHanaBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/young_hanabackend/YoungHanaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.young_hanabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungHanaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
