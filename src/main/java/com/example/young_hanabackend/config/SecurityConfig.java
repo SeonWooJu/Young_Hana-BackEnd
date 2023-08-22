0a9984df102fac9aba65c62231ed05bbb9462218
SeonWooJu <tjsdnwn21104@gmail.com>
2023-08-22 13:41:24 +0900

    Cors Error 해결

--- src/main/java/com/example/young_hanabackend/config/SecurityConfig.java
+++ src/main/java/com/example/young_hanabackend/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
